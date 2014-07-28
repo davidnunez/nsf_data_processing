fe7f5771c8fb2cb9a4e1b4fd327a0fc415f519ad
David Nunez <prg@ubuntu.(none)>
2014-07-28 14:24:52 -0400

    Changed to mp4 and higher bit rate

--- scripts/export.sh
+++ scripts/export.sh
@@ -3,8 +3,11 @@
 rosrun nsf_data_processing extract_mp3.py $1 $2 $3
 
 MP3="$1/$1.mp3"
-AVI="$1/$1.avi"
-AVI_NOSOUND="./$1/$1_nosound.avi"
+AVI="$1/$1.mp4"
+AVI_NOSOUND="./$1/$1_nosound.mp4"
 
-avconv -r 41040/1369 -i "$1/images/annotated/frame%010d.jpg" "$AVI_NOSOUND"
+#avconv -r 41040/1369 -b 65536k -i "$1/images/annotated/frame%010d.jpg" "$AVI_NOSOUND"
+avconv -r 41040/1369 -i "$1/images/annotated/frame%010d.jpg" -b 65536k -vsync cfr "$AVI_NOSOUND"
+
 avconv -i "$MP3" -i "$AVI_NOSOUND" -acodec copy -vcodec copy -map 0 -map 1:v "$AVI"
+
